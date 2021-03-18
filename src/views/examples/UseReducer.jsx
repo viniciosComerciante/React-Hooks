05366f5d7d3f1c0da8d033b29215470daed28074
vinicios <vinicompif@gmail.com>
2021-03-18 00:07:44 -0300

    iniciada aula sobre useReducer

--- src/views/examples/UseReducer.jsx
+++ src/views/examples/UseReducer.jsx
@@ -1,6 +1,14 @@
 import React from 'react'
 import PageTitle from '../../components/layout/PageTitle'
 
+const initialState = {
+    cart: [],
+    products: [],
+    user: null,
+    //foco
+    number: 0
+}
+
 const UseReducer = (props) => {
     return (
         <div className="UseReducer">
